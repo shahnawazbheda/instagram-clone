a4f06f4d41140766d61a19aa8311e0552fe72872
Shahnawaz-technocomet <shahnawazbheda@gmail.com>
2025-06-25 18:18:17 +0530

    update

--- Instagram-First-project/config/db.config.js
+++ Instagram-First-project/config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    connectionString: 'mongodb+srv://shahnawazbheda:ShahnawazBheda@cluster0.uad7zn6.mongodb.net/socialmedia',
-    portNo: 8080
+    connectionString: process.env.MONGODB_URI || 'mongodb+srv://shahnawazbheda:ShahnawazBheda@cluster0.uad7zn6.mongodb.net/socialmedia',
+    portNo: process.env.PORT || 8080
 };
